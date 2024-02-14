f56220dcf49601011d37950f402798ef9deeb624
nurbekkhujaev <nurbek.khuja@gmail.com>
2024-02-14 17:25:36 +0500

    updated

--- src/main/java/com/epam/mjc/io/FileReader.java
+++ src/main/java/com/epam/mjc/io/FileReader.java
@@ -1,11 +1,54 @@
 package com.epam.mjc.io;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.IOException;
 
 
 public class FileReader {
 
     public Profile getDataFromFile(File file) {
-        return new Profile();
+
+        try (BufferedReader bufferedReader = new BufferedReader(new java.io.FileReader(file))) {
+
+            String name = null;
+            Integer age = null;
+            String email = null;
+            Long phone = null;
+
+            String line;
+
+            while ((line = bufferedReader.readLine()) != null) {
+                String[] parts = line.split(": ", 2);
+                if (parts.length == 2) {
+                    String key = parts[0].trim();
+                    String value = parts[1].trim();
+                    switch (key) {
+                        case "Name":
+                            name = value;
+                            break;
+                        case "Age":
+                            age = Integer.parseInt(value);
+                            break;
+                        case "Email":
+                            email = value;
+                            break;
+                        case "Phone":
+                            phone = Long.parseLong(value);
+                            break;
+                    }
+                }
+            }
+
+            return new Profile(name, age, email, phone);
+        } catch (IOException e) {
+            e.printStackTrace();
+            return null;
+        }
     }
+
+
 }
+
+
+
